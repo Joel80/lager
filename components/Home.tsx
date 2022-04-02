89a2dc9d6bf2c493f1ec126987e6812f5da54cd8
Joel Löfgren <jdtlofgren@gmail.com>
2022-04-02 23:49:08 +0200

    New component for Home view

---
+++ components/Home.tsx
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Stock from './Stock';
+import greenhouse from '../assets/greenhouse.jpg';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollStyle}>
+        <View style={styles.base}>
+          <Text style={styles.mainHeading}>Infinity Warehouse</Text>
+          <Text style={styles.subHeading}>Trädgård</Text>
+          <Image source={greenhouse} style={styles.image} />
+          <Text style={styles.imageCredit}>Photo by Christin Noelle at Unsplash</Text>
+          <Stock />
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollStyle: {
+    backgroundColor: '#357960'
+  },
+  base: {
+    flex: 1,
+    /* alignItems: 'center', */
+    paddingLeft: 24,
+    paddingRight: 24
+  },
+  mainHeading: {
+    color: '#fdfdfd', 
+    fontSize: 36,
+    paddingTop: 16
+    /* textAlign: 'center', */ 
+  },
+  subHeading: {
+    color: '#fdfdfd', 
+    fontSize: 28, 
+    paddingBottom: 16,
+    /* textAlign: 'center' */
+  },
+  image: {
+    width: 320, 
+    height: 240,
+  },
+  imageCredit: {
+    fontSize: 14,
+    color: '#fdfdfd',
+    paddingTop: 2
+  }
+});
