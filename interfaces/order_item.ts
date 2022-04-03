06923385050b79124803325eac71b2251401f892
Joel Löfgren <jdtlofgren@gmail.com>
2022-04-03 22:44:51 +0200

    Changed interface of order_item

--- interfaces/order_item.ts
+++ interfaces/order_item.ts
@@ -1,11 +1,5 @@
-// Interface for orderItem, defines the properties and
-    // their types for an orderItem
-    export default interface OrderItem {
-        name: string; 
-        stock: number; 
-        article_number: string; 
-        description: string; 
-        specifiers: string; 
-        location: string; 
-        price: number;
-    }
+export default interface OrderItem {
+    order_id: number,
+    product_id: number,
+    amount: number,
+};
