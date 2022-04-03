febc7864a09492c6a8b8975c3b69870af150770b
Joel Löfgren <jdtlofgren@gmail.com>
2022-04-03 22:45:16 +0200

    Added interface for product

---
+++ interfaces/product.ts
@@ -0,0 +1,10 @@
+
+export default interface Product {
+    name: string; 
+    stock: number; 
+    article_number: string; 
+    description: string; 
+    specifiers: string; 
+    location: string; 
+    price: number;
+};
