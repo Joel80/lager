857c25153dbccacd649a221bed4df65b9bad1f7d
Joel Löfgren <jdtlofgren@gmail.com>
2022-04-03 17:04:50 +0200

    Added styles for Typography and Base

---
+++ styles/base.js
@@ -0,0 +1,23 @@
+export const base = {
+    flex: 1,
+    paddingLeft: 24,
+    paddingRight: 24
+};
+
+export const container = {
+    flex: 1,
+};
+
+export const mainBackgroundColor = {
+    backgroundColor: '#357960',
+};
+
+
+export const mainTextColor = {
+    color: '#fdfdfd',
+};
+
+export const image = {
+    width: 320,
+    height: 240,
+};
---
+++ styles/typography.js
@@ -0,0 +1,25 @@
+export const header1 = {
+    fontSize: 38,
+    marginBottom: 28,
+};
+
+export const header2 = {
+    fontSize: 32,
+    marginBottom: 28,
+};
+
+export const header3 = {
+    fontSize: 24,
+    marginBottom: 28,
+};
+
+export const normal = {
+    fontSize: 18,
+    marginBottom: 28,
+};
+
+export const imageCredit = {
+    fontSize: 14,
+    marginTop: 7,
+    marginBottom: 28,
+}
