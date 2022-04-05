2cfd25981e0e34b0361216ed907cdcd3aa22ec3e
Joel Löfgren <jdtlofgren@gmail.com>
2022-04-05 02:29:04 +0200

    Begun work on pickOrder function

--- models/orders.ts
+++ models/orders.ts
@@ -1,5 +1,7 @@
 import config from '../config/config.json';
 import Order from '../interfaces/order';
+import productModel from './products';
+import Product from '../interfaces/product'
 
 const orders = {
     getOrders: async function getOrders() {
@@ -8,11 +10,19 @@ const orders = {
 
         return result.data;
     },
-    pickOrder: async function pickOrder(order: Partial<Order>) {
-        //TODO: Minska lagesaldo för de orderrader
+    pickOrder: async function pickOrder(order: Order) {
+        //TODO: Minska lagersaldo för de orderrader
         // som finns i ordern
+        for (const item of order.order_items) {
+            const product: Product = await productModel.getProduct(item.product_id);
+            //console.log(product);
+            await productModel.updateStock(product, item.amount);
+        }
+        
+
 
         //TODO: Ändra status för ordern som är packad
+
     }
 };
 
