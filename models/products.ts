d7c16c874baa6998451b1ee0dd39a337cccafd08
Joel Löfgren <jdtlofgren@gmail.com>
2022-04-06 23:40:37 +0200

    Added function for updating products and getting all products

--- models/products.ts
+++ models/products.ts
@@ -5,10 +5,17 @@ import Product from '../interfaces/product';
 
 
 const products = {
+  
     getProduct: async function getProduct(product_id: number) {
         const response = await fetch(`${config.base_url}/products/${product_id}?api_key=${config.api_key}`);
         const result = await response.json();
         //console.log(result.data);
+
+        return result.data;
+    },
+    getAllProducts: async function getAllProducts() {
+        const response = await fetch(`${config.base_url}/products/?api_key=${config.api_key}`);
+        const result = await response.json();
 
         return result.data;
     },
@@ -18,16 +25,29 @@ const products = {
         //console.log(JSON.stringify(product));
         product.api_key = config.api_key;
         console.log(product);
-        fetch(`https://lager.emilfolino.se/v2/products`, {
+        await fetch(`${config.base_url}/products`, {
             body: JSON.stringify(product),
             headers: {
-            'content-type': 'application/json'
-        },
-        method: 'PUT'
-        })
-        .then(function (response) {
-            console.log(response);
+                'content-type': 'application/json'
+            },
+            method: 'PUT'
         });
+    },
+    updateProduct: async function updateProduct(product: Product) {
+        product.api_key = config.api_key;
+        try {
+            await fetch(`${config.base_url}/products`, {
+                body: JSON.stringify(product),
+                headers: {
+                    'content-type': 'application/json'
+                },
+                method: 'PUT'
+            });
+
+        } catch (error) {
+            console.log("Could not update product");
+            console.log(error);
+        }       
     }
 };
 export default products;
