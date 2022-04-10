30f025b4eb7d1ed0debd5fe0a26bcf0bac43af39
Joel Löfgren <jdtlofgren@gmail.com>
2022-04-10 22:27:14 +0200

    Added styles for forms

---
+++ styles/forms.js
@@ -0,0 +1,10 @@
+export const input = {
+    fontSize: 20,
+    marginBottom: 28,
+    borderWidth: 1,
+    padding: 5,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    backgroundColor: '#f2f0e6',
+    color: '#357960',
+};
--- styles/index.js
+++ styles/index.js
@@ -1,5 +1,6 @@
 import * as Base from './base';
 import * as Typography from './typography';
 import * as ButtonStyle from './button';
+import * as Forms from './forms';
 
-export { Base, Typography, ButtonStyle}
+export { Base, Typography, ButtonStyle, Forms}
