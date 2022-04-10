da201819078f53b0c9eb1f408c422b9d3170958a
Joel Löfgren <jdtlofgren@gmail.com>
2022-04-10 09:12:36 +0200

    Changed way of creating product in function pickOrder, updateOrder now has Partial<Order> parameter

--- models/orders.ts
+++ models/orders.ts
@@ -1,7 +1,8 @@
 import config from '../config/config.json';
 import Order from '../interfaces/order';
 import productModel from './products';
-import Product from '../interfaces/product'
+import Product from '../interfaces/product';
+import OrderItem from '../interfaces/product';
 
 const orders = {
     getOrders: async function getOrders(): Promise<Order[]> {
@@ -13,10 +14,19 @@ const orders = {
     pickOrder: async function pickOrder(order: Order) {
         // Remove products from stock
         for (const item of order.order_items) {
-            const product: Product = await productModel.getProduct(item.product_id);
+            
+            const newStock = item.stock - item.amount;
+
+            let partialProduct: Partial<Product> = {
+                name: item.name,
+                id: item.product_id,
+                stock: newStock,
+            }
+             
+            //const product: Product = await productModel.getProduct(item.product_id);
             //console.log(product);
-            product.stock -= item.amount; 
-            await productModel.updateProduct(product);
+            //product.stock -= item.amount; 
+            await productModel.updateProduct(partialProduct);
         }
         
         // Change order status to 200
@@ -34,7 +44,7 @@ const orders = {
         method: 'PUT'
         });
     },
-    updateOrder: async function updateOrder(order: Order) {
+    updateOrder: async function updateOrder(order: Partial<Order>) {
         order.api_key = config.api_key;
         try {
 
