08b65a977b1aa178e527fc8b27ad9dcdea34db6b
Joel Löfgren <jdtlofgren@gmail.com>
2022-04-13 00:16:20 +0200

    Worked on style for datepicker

--- styles/datePicker.js
+++ styles/datePicker.js
@@ -1,8 +1,11 @@
+import { setStatusBarHidden } from "expo-status-bar";
+
 export const datePickerStyle = {
     backgroundColor: '#f2f0e6',
+    overflow: 'hidden',
     marginBottom: 24,
     borderWidth: 1,
     borderColor: "#ccc",
     borderRadius: 8,
-    padding: 5
+    /* padding: 10, */
 };
