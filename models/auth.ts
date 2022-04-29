f711c17754d7d63f6da56257e9eee6237f9bef2c
Joel Löfgren <jdtlofgren@gmail.com>
2022-04-30 01:13:21 +0200

    Added flashmessages for login

--- models/auth.ts
+++ models/auth.ts
@@ -27,11 +27,21 @@ const auth = {
         });
         const result = await response.json();
 
-        console.log(result);
+        if (Object.prototype.hasOwnProperty.call(result, 'errors')) {
+            return {
+                title: result.errors.title,
+                message: result.errors.detail,
+                type: "danger",
+            };
+        }
         
         await storage.storeToken(result.data.token);
 
-        return result.data.message
+        return {
+            title: "Inloggning",
+            message: result.data.message,
+            type:  <'success'> "success",
+        };
     },
     register: async function register(email: string, password: string) {
         const data = {
