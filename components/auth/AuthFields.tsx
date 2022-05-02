d87cafce13b1630155d4732b7a8e116823e4b440
Joel Löfgren <jdtlofgren@gmail.com>
2022-05-03 00:15:11 +0200

    Implemented functions with regex to validate fields

--- components/auth/AuthFields.tsx
+++ components/auth/AuthFields.tsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
 import { ScrollView, Text, TextInput, Pressable } from "react-native";
+import { showMessage } from "react-native-flash-message";
 import { Typography, Forms, Base, ButtonStyle} from '../../styles';
 
 export default function AuthFields ({auth, setAuth, title, submit, navigation }) {
+    const [validPassword, setValidPassword] = useState<Boolean>(false);
+    const [validEmail, setValidEmail] = useState<Boolean>(false);
+
+    function validatePassword (text: string) {
+        let validPassword = false;
+        const pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!\.-]).{4,}$/
+        if (text.match(pattern)) {
+            validPassword = true;
+            
+        } else {
+            showMessage({
+                message: "Icke giltigt lösenord",
+                description: "Lösenordet måste innehålla minst 4 tecken, små och stora bokstäver och ett specialtecken",
+                type: "warning"
+            });
+            
+            validPassword = false;
+        }
+
+        setValidPassword(validPassword);
+    }
+
+    function validateEmail (text: string) {
+        let validEmail = false;
+        const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if (text.match(pattern)) {
+            validEmail = true;
+        } else {
+            showMessage({
+                message: "Icke giltig epostadress",
+                description: "E-post måste uppfylla cccc@cc.cc",
+                type: "warning"
+            });
+
+            validEmail = false;
+        }
+
+        setValidEmail(validEmail);
+    }
+
     return (
     <ScrollView style={[Base.base, Base.mainBackgroundColor]}>
         <Text style={[Typography.header2, Base.mainTextColor]}>{title}</Text>
@@ -9,6 +51,7 @@ export default function AuthFields ({auth, setAuth, title, submit, navigation })
         <TextInput            
             style={Forms.input}
             onChangeText={(content: string) =>  {
+                validateEmail(content);
                 setAuth({...auth, email: content})
             }}
             autoCapitalize="none"
@@ -21,6 +64,7 @@ export default function AuthFields ({auth, setAuth, title, submit, navigation })
         <TextInput
             style={Forms.input}
             onChangeText={(content: string) =>  {
+                validatePassword(content);
                 setAuth({...auth, password: content})
             }}
             value={auth?.password}
@@ -30,7 +74,15 @@ export default function AuthFields ({auth, setAuth, title, submit, navigation })
         />
         <Pressable style={() => [{}, ButtonStyle.button]}
             onPress= { () => {
-                submit();
+                if(validPassword && validEmail) {
+                    submit();
+                } else {
+                    showMessage({
+                        message: "Icke giltig epostadress eller lösenord",
+                        type: "danger"
+                    });
+                }
+                
             }}>
             <Text style={ButtonStyle.buttonText}>{title}</Text>
         </Pressable>
